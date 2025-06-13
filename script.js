2263f641a3aec630e58bd29b50566278b7e1a7bf
nagesh nayak <nayak10nagesh@gmail.com>
2025-06-14 00:32:31 +0530

    Bg Added

--- script.js
+++ script.js
@@ -580,3 +580,55 @@ function showUserTopScores() {
     console.error("Failed to load user scores:", error);
   });
 }
+
+
+let banner =  document.querySelector('.container')
+         let canvas = document.getElementById("dotsCanvas")
+         canvas.width=canvas.offsetWidth;
+         canvas.height=canvas.offsetHeight;
+         let ctx=canvas.getContext('2d');
+
+     let dots=[]
+     let arraycolor=['#eee','#545454','#596d91','#bb5a68','#5a5b54']
+     for (let index = 0; index < 50; index++) {
+        dots.push({
+            x:Math.floor(Math.random()*canvas.width),
+            y:Math.floor(Math.random()*canvas.height),
+            size:Math.random()*3+5,
+            color:arraycolor[Math.floor(Math.random()*5)]
+
+        });}
+      const drawDots = () => {
+        dots.forEach(dot =>{
+            ctx.fillStyle = dot.color;
+            ctx.beginPath();
+            ctx.arc(dot.x,dot.y,dot.size,0,Math.PI*2);
+            ctx.fill()
+        })
+     }
+
+    drawDots();
+  banner.addEventListener('mousemove',(e)=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    drawDots();
+    let mouse={
+        x:e.pageX - banner.getBoundingClientRect().left,
+        y:e.pageY - banner.getBoundingClientRect().top
+    }
+    dots.forEach(dot=>{
+       let distance = Math.sqrt((mouse.x - dot.x)**2 + (mouse.y - dot.y)**2);
+       if(distance<300){
+          ctx.strokeStyle=dot.color;
+          ctx.lineWidth=1;
+          ctx.beginPath();
+          ctx.moveTo(dot.x,dot.y)
+          ctx.lineTo(mouse.x,mouse.y)
+          ctx.stroke();
+       }
+    })
+  })
+ banner.addEventListener('mouseout',()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    drawDots()
+ })
+ 
